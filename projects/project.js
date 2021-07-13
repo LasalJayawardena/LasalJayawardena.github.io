a16670fdcc92aaf546ed42e811f6382083a00fcd
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-07-13 13:00:58 +0530

    Added Personalization to projects

--- projects/project.js
+++ projects/project.js
@@ -5,3 +5,23 @@ if (theme == null) {
 } else {
     setTheme(theme);
 }
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = '../default.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = '../blue.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = '../green.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = '../purple.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
